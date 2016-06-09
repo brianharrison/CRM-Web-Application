e0c5d26f9186b7729954d3364d26bdffc03018b4
Brian Harrison <brianharrison@rogers.com>
2016-06-09 17:45:00 -0400

    Created fake contacts and requested all contacts

--- crm.rb
+++ crm.rb
@@ -1,5 +1,18 @@
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+	@crm_app_name = "Brian's CRM"
   erb :index
 end
+
+get '/contacts' do
+
+	erb :contacts
+
+end
+
+	Contact.create('Mickey', 'Mouse', 'mickey@disney.com', 'mouse')
+	Contact.create('Donald', 'Duck', 'donald@disney.com', 'duck')
+	Contact.create('Wily', "Coyote", 'wily@wb.com', 'coyote')
+	Contact.create('Daffy', 'Duck', 'daffy@wb.com', 'duck')
